241e3178d98e251adc7f206fb418a8f1486b95e1
bmw1975487 <zohomail343@gmail.com>
2026-08-31 21:31:35 +0300

    Make Gothic first and primary mode

--- roomvision_gothic_demo/app/src/main/java/com/roomvision/demo/FilterType.java
+++ roomvision_gothic_demo/app/src/main/java/com/roomvision/demo/FilterType.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum FilterType {
+    GOTHIC("Gothic / Dracula"),
     MATRIX("Matrix"),
     CRAYON("Crayon"),
     BLUE_PEN("Blue Pen"),
-    ASCII("ASCII"),
-    GOTHIC("Gothic");
+    ASCII("ASCII");
 
     public final String label;
     FilterType(String label) { this.label = label; }
